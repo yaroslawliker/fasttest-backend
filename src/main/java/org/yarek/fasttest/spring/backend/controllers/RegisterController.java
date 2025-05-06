4c7163db090a10b1aaaf18c8ae8d05f2a7edcbce
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-06 20:52:50 +0300

    Added: RegisterController Post now uses UserService

--- src/main/java/org/yarek/fasttest/spring/backend/controllers/RegisterController.java
+++ src/main/java/org/yarek/fasttest/spring/backend/controllers/RegisterController.java
@@ -1,15 +1,24 @@
 package org.yarek.fasttest.spring.backend.controllers;
 
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.yarek.fasttest.spring.backend.dto.SignupRequest;
+import org.yarek.fasttest.spring.backend.services.UserService;
 
 @RestController
 @RequestMapping("/signup")
 public class RegisterController {
 
+    private final UserService userService;
+
+    @Autowired
+    public RegisterController(UserService userService) {
+        this.userService = userService;
+    }
+
     @PostMapping
     public void signupUser(@Valid @RequestBody SignupRequest signupRequest) {
-        // TODO: use service to save user
+        userService.saveUser(signupRequest);
     }
 }
