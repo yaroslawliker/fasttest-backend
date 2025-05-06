82603eb6b57ac84822c792d44386cc669a533a7c
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-06 18:57:41 +0300

    Initial commit

---
+++ src/test/java/org/yarek/fasttest/spring/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.yarek.fasttest.spring.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
