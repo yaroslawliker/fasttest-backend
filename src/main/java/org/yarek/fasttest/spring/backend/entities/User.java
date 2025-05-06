b784e4bba1d09725e6119bce4e3d8963b9e58aea
Yaroslav Vorobiov <yaroslawliker@gmail.com>
2025-05-06 20:55:59 +0300

    Added: getters and setter to user

--- src/main/java/org/yarek/fasttest/spring/backend/entities/User.java
+++ src/main/java/org/yarek/fasttest/spring/backend/entities/User.java
@@ -34,6 +34,38 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
     @Column(nullable = false)
     private Role role;
 
